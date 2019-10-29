dfedd3c65ad956703b18e069b9cd12d4f58ac8f5
Brian Nicholls <nichol88@gmail.com>
2019-10-29 05:54:06 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -5,13 +5,12 @@ end
 
 def find_max_value(array)
   # Add your solution here
-  puts array
-  puts array.sort
-  array.last()
+   b = array.sort
+  b.last()
 end
 
 def find_min_value(array)
   # Add your solution here
-  array.sort
-  array.last
+  b = array.sort
+  b.last
 end
